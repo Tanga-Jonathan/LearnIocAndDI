3a3d4ea9656a87b5103eacbdd2306ae17e627fc1
Tanga-Jonathan <jonathantanga97@gmail.com>
2025-10-15 04:28:53 +0300

    ioc first commit

---
+++ src/main/java/com/kamajo/simpleIoc/SimpleIocApplication.java
@@ -0,0 +1,13 @@
+package com.kamajo.simpleIoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleIocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamajo/simpleIoc/SimpleIocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamajo.simpleIoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleIocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
